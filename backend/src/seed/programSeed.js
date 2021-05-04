597e25bfea8395a83e474f0397dffeae74356cbb
joeelo <44529192+joeelo@users.noreply.github.com>
2021-05-04 12:04:13 -0700

    readme style changes for mike

--- backend/src/seed/programSeed.js
+++ backend/src/seed/programSeed.js
@@ -7,5 +7,37 @@ module.exports = [
     coverImage: '', 
     tags: ['code', 'program', 'summer', 'teaching'],
     organization: 'Mission Bit'
-  }
+  }, 
 ]
+
+/*
+  SAMPLE OBJECT 
+  {
+    key: 'value',
+  },
+  
+  The key has no quotes, followed by a colon, and the value has ' ' marks around it. 
+  The line is finished with a comma, and then go on to the next line. 
+  
+  things that start with [] are arrays, they start with [ and then input a string that are comma separated ie
+  ['string1', 'string2', 'string3'],
+  
+  edge cases
+  If you want to use an ' in a word, please use an escape character first which is \
+  So it would look like 'I\'m going to code for fun'.
+  
+  You can fill out this templatized object below.
+  {
+    title: '', 
+    content: '',
+    bio: '',
+    helpsWith: [''],
+    coverImage: '', 
+    tags: [''], 
+    organization: ''
+  },
+  
+  You can run copy whole file and run through https://esprima.org/demo/validate.html to see if it's valid. 
+  If you have any questions of course ask, 
+  Happy Coding! 
+*/
