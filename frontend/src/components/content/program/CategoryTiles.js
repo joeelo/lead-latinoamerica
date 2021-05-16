e5aea651049ade5983d70963aba879f292b630fe
joseph.lorenzo <joseph.lorenzo@avast.com>
2021-05-16 01:54:48 -0700

    almost done with overview page need to go back to the mocks

---
+++ frontend/src/components/content/program/CategoryTiles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const CategoryTiles = ({ adjectives }) => {
+  return (
+    <Container>
+    
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%; 
+  max-width: 300px; 
+  display: flex; 
+  flex-wrap: wrap; 
+  margin-top: 40px; 
+`
+
+const Tile = styled.div`
+  height: 50px; 
+`
--- frontend/src/components/content/program/ProgramOverviewAndInfo.js
+++ frontend/src/components/content/program/ProgramOverviewAndInfo.js
@@ -1,11 +1,61 @@
 import CenterFlexContainer from "@/components/generic/CenterFlexContainer";
+import styled from 'styled-components';
 
-const ProgramOverviewAndInfo = () => {
+const ProgramOverviewAndInfo = ({ program }) => {
   return (
     <CenterFlexContainer marginTop={ true }>
-      
+      <LeftContainer>
+        <LargeText> Overview </LargeText>
+        <OverviewText> { program.overview } </OverviewText>
+      </LeftContainer>
+      <RightContainer>
+        <UnderlinedSectionHeader> Categories </UnderlinedSectionHeader>
+        
+        <UnderlinedSectionHeader> Start Date </UnderlinedSectionHeader>
+        <StartDateText> { program.startData ? program.startData : 'TBD' } </StartDateText>
+
+      </RightContainer>
     </CenterFlexContainer>
   )
 }
 
 export default ProgramOverviewAndInfo;
+
+const LargeText = styled.h2`
+  font-weight: bold;
+  font-size: 72px; 
+  margin-bottom: 40px; 
+`
+
+const InnerContainer = styled.div`
+  padding: 10px 20px; 
+  display: flex; 
+  flex-direction: column; 
+`
+
+const LeftContainer = styled(InnerContainer)`
+  width: 60%; 
+`
+
+const RightContainer = styled(InnerContainer)`
+  width: 40%; 
+`
+
+const MidSizeText = styled.p`
+  font-size: 26px; 
+  line-height: 32px
+`
+
+const OverviewText = styled(MidSizeText)`
+  font-size: 26px;
+`
+
+const StartDateText = styled(MidSizeText)`
+  font-size: 26px;
+`
+
+const UnderlinedSectionHeader = styled.h3`
+  font-size: 32px; 
+  text-decoration: underline; 
+`
+
