358c5e2c0879c700238e1d22d527f80bc1f5f500
joseph.lorenzo <joseph.lorenzo@avast.com>
2021-06-08 17:04:46 -0700

    updated link

---
+++ frontend/src/components/generic/LinkUnderlineEffect.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+
+const LinkUnderlineEffect = ({ text, hrefFormatted, hrefAs }) => {
+  return (
+    <LinkUnderlineEffect>
+    
+    </LinkUnderlineEffect>
+  )
+}
+
+export default LinkUnderlineEffect;
+
