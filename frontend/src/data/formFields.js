2bb5be92ee8b8e128a5d673862e46aa696e2d225
Joe Lorenzo <josephclorenzo@gmail.com>
2021-07-23 22:09:11 -0700

    updated form

--- frontend/src/data/formFields.js
+++ frontend/src/data/formFields.js
@@ -2,13 +2,12 @@ export default {
   addProgram: {
       formTitle: 'Add Program', 
       list: [
-      { label: 'title', placeHolder: 'what is the name of the program?' }, 
-      { label: 'content', placeHolder: 'what purpose do they serve?' }, 
-      { label: 'bio', placeHolder: 'Tell us a bit about the program' }, 
-      { label: 'organization', placeHolder: 'Who runs this program?' }, 
-      { label: 'helpsWith', placeHolder: 'Please put a list of comma separated adjectives' }, 
-      { label: 'coverImage', placeHolder: 'A URL to the program image wanted' }, 
-      { label: 'tags', placeHolder: 'Keywords that may help one locate this program' },
+      { label: 'Title', placeHolder: 'what is the name of the program?' }, 
+      { label: 'Content', placeHolder: 'what purpose do they serve?' }, 
+      { label: 'Bio', placeHolder: 'Tell us a bit about the program' }, 
+      { label: 'How you can help', placeHolder: 'Please put a list of comma separated adjectives' }, 
+      { label: 'Org Image', placeHolder: 'A URL to the program image wanted' }, 
+      { label: 'Tags', placeHolder: 'Keywords that may help one locate this program' },
     ]
   }
 }
