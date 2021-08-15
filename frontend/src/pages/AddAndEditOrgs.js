7a29f04535fe2594fab204acddb77210dc428abb
joeelo <josephclorenzo@gmail.com>
2021-08-15 10:25:57 -0700

    add

--- frontend/src/pages/AddAndEditOrgs.js
+++ frontend/src/pages/AddAndEditOrgs.js
@@ -1,6 +1,6 @@
+import styled from 'styled-components';
 import formFields from '@/data/formFields';
 import Form from '@/components/form/Form';
-import styled from 'styled-components';
 import CenterFlexContainer from '@/components/generic/CenterFlexContainer';
 import NavBar from '@/components/nav/NavBar';
 import Footer from '@/components/footer/Footer';
