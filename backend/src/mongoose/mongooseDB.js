992bcc1325bdbdb4c712a625a54054b9023ea0ed
joeelo <josephclorenzo@gmail.com>
2021-08-16 23:46:23 -0700

    again

--- backend/src/mongoose/mongooseDB.js
+++ backend/src/mongoose/mongooseDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const server =`'mongodb+srv://joeeloee:'${process.env.DB_PASSWORD}@cluster0.nnv78.mongodb.net/lead-latinoamerica?retryWrites=true&w=majority`; 
+const server = `mongodb+srv://joeeloee:${process.env.DB_PASSWORD}@cluster0.nnv78.mongodb.net/lead-latinoamerica?retryWrites=true&w=majority`; 
 const database = "lead-latinoamerica"; 
 
 
