5bd8ad37fe39318a10f5f26aa49fce88c4df9e01
joeelo <josephclorenzo@gmail.com>
2021-08-16 22:18:12 -0700

    should be working on vercel production

--- frontend/pages/resources/[resourceSlug]/[programSlug]/index.js
+++ frontend/pages/resources/[resourceSlug]/[programSlug]/index.js
@@ -1,16 +1,15 @@
-import NavBar from '@/components/nav/NavBar';
-import Footer from '@/components/footer/Footer';
-import SkewedTitleAndPhoto from '@/components/content/program/SkewedTitleAndPhoto';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import fakeData from '@/data/fakeData';
-// import ProgramOverViewAndInfo from '@/components/content/program/ProgramOverViewAndInfo';
+import NavBar from '@/components/nav/NavBar';
+import Footer from '@/components/footer/Footer';
+import SkewedTitleAndPhoto from '@/components/content/program/SkewedTitleAndPhoto';
+import ProgramOverViewAndInfo from '@/components/content/program/ProgramOverViewAndInfo';
 
 const ProgramPage = () => {
   const router = useRouter(); 
   const [ program, setProgram ] = useState({ name: '' });
 
-  
   useEffect(() => {
     if ( !fakeData[router.query.resourceSlug] ) return;
     const programInfo = fakeData[router.query.resourceSlug].programs.find( p => p.href === router.query.programSlug )
@@ -23,7 +22,7 @@ const ProgramPage = () => {
     <>
       <NavBar />
       <SkewedTitleAndPhoto program={ program } router={ router }/>
-      {/* <ProgramOverViewAndInfo program={ program } marginTop={ true }/> */}
+      <ProgramOverViewAndInfo program={ program } marginTop={ true }/>
       <Footer marginTop={ true }/>
     </>
   )
