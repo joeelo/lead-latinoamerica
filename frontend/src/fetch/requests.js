36aa1e2d935db40a48f8cd865bd749c92a3bfdd0
joeelo <josephclorenzo@gmail.com>
2021-08-19 00:21:01 -0700

    in the process of patching organizational approval

--- frontend/src/fetch/requests.js
+++ frontend/src/fetch/requests.js
@@ -1,37 +1,55 @@
 const getProgramBySlug = async (endpoint) => {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_DB_LOCATION}/${endpoint}`); 
-    const json = await response.json(); 
-    return json;
-  } catch (error) {
-    console.log(error);
-    return { message: error };
-  }
+	try {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_DB_LOCATION}/${endpoint}`); 
+		const json = await response.json(); 
+		return json;
+	} catch (error) {
+		console.log(error);
+		return { message: error };
+	}
 }
 
 const postToDatabase = async (data, endpoint, query) => {
-  if (query.local) {
-    return { message: 'success' };
-  }
-  try {
-    const options = {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json'
-      }, 
-      body: JSON.stringify(data)
-    }
-    const response = await fetch(`${process.env.NEXT_PUBLIC_DB_LOCATION}/${endpoint}`, options);
-    const json = await response.json(); 
-    console.log('RESPONSE JSON FROM POSTTODATABASE: ', json);
-    return json; 
-  } catch (error) {
-    console.log(error);
-    return { message: error };
-  }
+	if (query.local) {
+		return { message: 'success' };
+	}
+	try {
+		const options = {
+			method: 'POST', 
+			headers: {
+				'Content-Type': 'application/json'
+			}, 
+			body: JSON.stringify(data)
+		}
+		const response = await fetch(`${process.env.NEXT_PUBLIC_DB_LOCATION}/${endpoint}`, options);
+		const json = await response.json(); 
+		console.log('RESPONSE JSON FROM POSTTODATABASE: ', json);
+		return json; 
+	} catch (error) {
+		console.log(error);
+		return { message: error };
+	}
+}
+
+const findProgramAndUpdate = async (data) => {
+	try {
+		const options = {
+			method: 'PATCH', 
+			headers: {
+				'Content-Type': 'application/json'
+			}, 
+			body: JSON.stringify(data)
+		}
+		const request = await fetch(`${process.env.NEXT_PUBLIC_DB_LOCATION}/${endpoint}`, options);
+		const json = request.json(); 
+		console.log(json);
+		return json; 
+	} catch (error) {
+		console.log('ERROR IN FINDPROGRAMANDUPDATE: ', error);
+	}
 }
 
 export {
-  getProgramBySlug, 
-  postToDatabase
+	getProgramBySlug, 
+	postToDatabase
 }
