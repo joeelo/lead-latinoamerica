ab086a0de8dd84631f2779280b58db87824f2d7f
joeelo <44529192+joeelo@users.noreply.github.com>
2021-11-18 21:49:25 -0800

    Sign in page (#8)
    * started sign in page
    
    * sign-up page almost finished

---
+++ frontend/pages/sign-in/index.js
@@ -0,0 +1,116 @@
+import { useContext } from 'react';
+import styled, { ThemeContext } from 'styled-components';
+import Footer from "@/components/footer/Footer";
+import NavBar from "@/components/nav/NavBar";
+import ChangingBackgroundText from '@/components/content/ChangingBackgroundText';
+import Button from '@/components/generic/Button';
+import LinkButton from '@/components/generic/LinkButton';
+
+const Signup = () => {
+
+  const theme = useContext(ThemeContext);
+
+  return (
+    <>
+      <NavBar />
+        <Container>
+          <PhotoWithText>
+            <Title>
+              Sign in to <br/>
+              Lead With Us
+            </Title>
+          </PhotoWithText>
+
+          <Column>
+            <ChangingBackgroundText 
+              initialColor={ theme.colors.cultured }
+              secondaryColor={ theme.colors.darkBlue }
+              text={ 'Sign In' }
+              fontColorInitial={ theme.colors.darkBlue }
+              fontColorSecondary={ theme.colors.cultured }
+              maxWidth='400px'
+              onlyRunOneTransition
+            />
+            <LoginButton>
+              <GoogleLogo src='/images/google-logo.png'/>
+              Sign in with Google
+            </LoginButton>
+            <Span>Don't have an account?</Span>
+            <LinkButton 
+              text='Sign Up Now' 
+              hrefFormatted='/sign-up'
+              hrefAs='/sign-up'
+            />
+          </Column>
+        </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default Signup; 
+
+const Container = styled.div`
+  display: flex; 
+`
+
+const PhotoWithText = styled.div`
+  width: 60%; 
+  min-height: 700px; 
+  background-image: url('/images/library-unsplash.jpg');
+  background-size: cover; 
+  background-position: center center; 
+  background-repeat: no-repeat; 
+  display: flex; 
+  justify-content: center; 
+
+  @media screen and (max-width: 768px) {
+
+  }
+`
+
+const Title = styled.h2`
+  font-size: 64px; 
+  font-weight: 800; 
+  color: white; 
+  text-align: center; 
+  margin-top: 150px;
+`
+
+const Column = styled.div`
+  flex-direction: column; 
+  align-items: center;
+  padding-top: 150px; 
+  width: 40%; 
+  display: flex; 
+
+`
+
+const LoginButton = styled(Button)`
+  background-color: white; 
+  color: #222;
+  width: 400px;
+  border: 1px solid #999;
+  box-shadow: 3px 8px 9px 0px rgba(184,177,184,1);
+  height: 80px;
+  position: relative; 
+  max-width: 90%;
+  
+  &:hover {
+    box-shadow: 3px 5px 6px -2px rgba(184,177,184,1);
+  }
+`
+
+const GoogleLogo = styled.img`
+  max-width: 30px; 
+  max-height: 30px; 
+  position: absolute; 
+  left: 10px;
+  top: 25px;
+`
+
+const Span = styled.span`
+  margin-top: 40px; 
+  font-weight: bold; 
+  font-size: 18px; 
+`
---
+++ frontend/src/components/generic/Button.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Button = styled.div`
+  height: 60px; 
+  width: 250px; 
+  font-size: 26px; 
+  display: flex; 
+  align-items: center; 
+  text-align: center;
+  justify-content: center;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 40px;
+  transition: .2s ease-in-out;
+  max-width: 90%; 
+  
+`
+
+export default Button;
