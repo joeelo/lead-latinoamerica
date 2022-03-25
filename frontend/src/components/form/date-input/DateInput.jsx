6613b2a340e781112b133abbda2eef546ae7018a
joeelo <josephclorenzo@gmail.com>
2022-03-24 23:29:36 -0700

    functional date input with error checks

--- frontend/src/components/form/date-input/DateInput.jsx
+++ frontend/src/components/form/date-input/DateInput.jsx
@@ -1,17 +1,58 @@
 import { useCallback, useEffect, useState } from 'react';
 import TextInput from '@/components/form/text-input/TextInput';
 
+const INVALID_MESSAGES = {
+  VALID_DATE: '',
+  INVALID_DAY: 'Please make sure you have a valid day range input',
+  INVALID_MONTH: 'Please make sure you have a valid month',
+  INVALID_YEAR: 'Please make sure the year is a valid year in the future',
+};
+
 const DateInput = ({ register, name, placeHolder, setValue, watch }) => {
   let date = watch(name);
   const [isBackspace, setIsBackspace] = useState(false);
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState({ showError: false, error: '' });
 
   const checkForDateValidity = (trueLength) => {
-    if (trueLength === 2) {
-      const month = date.slice(0, 2);
-      const dateObj = new Date(`2022-${month}-01`);
-      return dateObj;
+    if (!date) {
+      return null;
     }
+
+    const month = date.slice(0, 2);
+    const day = date.slice(3, 5);
+    const year = date.slice(6, 10);
+
+    if (trueLength === 8) {
+      const date = new Date(`${year}-${month}-${day}`);
+      if (!date) {
+        setIsValid({ showError: true, message: INVALID_MESSAGES.INVALID_YEAR });
+      } else if (date && isValid.showError) {
+        setIsValid({ showError: false, message: '' });
+      }
+    }
+
+    if (trueLength === 4) {
+      const date = new Date(`2022-${month}-${day}`);
+      if (!date) {
+        setIsValid({ showError: true, message: INVALID_MESSAGES.INVALID_DAY });
+      } else if (date && isValid.showError) {
+        setIsValid({ showError: false, message: '' });
+      }
+    }
+
+    if (trueLength === 2) {
+      const date = new Date(`2022-${month}-1`);
+      if (!date) {
+        setIsValid({
+          showError: true,
+          message: INVALID_MESSAGES.INVALID_MONTH,
+        });
+      } else if (date && isValid.showError) {
+        setIsValid({ showError: false, message: '' });
+      }
+    }
+
+    return null;
   };
 
   const formatDate = useCallback(() => {
@@ -20,9 +61,7 @@ const DateInput = ({ register, name, placeHolder, setValue, watch }) => {
     }
 
     const trueLength = date.replaceAll('/', '').length;
-    const isValid = checkForDateValidity(trueLength);
-
-    console.log('ISVALID: ', isValid);
+    checkForDateValidity(trueLength);
 
     if (trueLength > 8) {
       setValue(name, date.slice(0, 10));
