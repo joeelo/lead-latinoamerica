dc3d8c45dde0b19ada9d7580b3d07be97a4ad630
joeelo <josephclorenzo@gmail.com>
2023-01-01 23:08:23 -0800

    updated mobile hook

--- frontend/src/hooks/useIsMobile.js
+++ frontend/src/hooks/useIsMobile.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import debounce from 'lodash/debounce'
 
 const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false)
 
+  const debouncedFunc = useCallback(() => {
+    return setIsMobile(window.innerWidth < 768)
+  }, [isMobile])
+
   useEffect(() => {
-    const updateSize = () => {
-      setIsMobile(window.innerWidth < 768)
-    }
-    updateSize()
-
-    const debouncedResizer = debounce(updateSize, 250)
-
-    window.addEventListener('resize', debouncedResizer)
-
-    return () => window.removeEventListener('resize', debouncedResizer)
+    debouncedFunc()
   }, [])
+
+  useEffect(() => {
+    const debouncedCb = debounce(debouncedFunc, 250)
+    
+    window.addEventListener('resize', debouncedCb)
+
+    return () => {
+      debouncedCb.cancel()
+      window.removeEventListener('resize', debouncedCb)
+    }
+  }, [isMobile, debouncedFunc])
 
   return isMobile
 }
