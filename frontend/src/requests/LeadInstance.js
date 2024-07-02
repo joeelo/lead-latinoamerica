c643800f8d8e6f6e7e6ba992fa40561d9419fafa
joeelo <josephclorenzo@gmail.com>
2024-07-01 22:04:30 -0700

    saving

--- frontend/src/requests/LeadInstance.js
+++ frontend/src/requests/LeadInstance.js
@@ -1,5 +1,15 @@
 import axios from 'axios'
 
+// https://axios-http.com/docs/instance
 export const LeadInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_DB_LOCATION, 
+})
+
+// https://axios-http.com/docs/interceptors
+LeadInstance.interceptors.request.use(function(response) {
+  return response
+}, function(error) {
+  console.error('API ERROR: ', error)
+
+  return Promise.reject(error)
 })
